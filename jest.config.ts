7aa943858f6238f0b7f6348deeb8b67014583867
Gleyson Abreu <gleyson_datu@hotmail.com>
2022-12-07 11:17:51 -0300

    jest: add collect coverage from options

--- jest.config.ts
+++ jest.config.ts
@@ -4,7 +4,16 @@ export default {
 
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/app.ts',
+    '!<rootDir>/src/index.ts',
+    '!<rootDir>/src/modules/**/useCases/**/index.ts',
+    '!<rootDir>/src/ports/**/*.ts',
+    '!<rootDir>/src/@types/*.d.ts',
+    '!<rootDir>/src/lib/*.ts',
+    '!<rootDir>/src/modules/**/infra/**/*.ts'
+  ],
   testEnvironment: 'node',
   preset: 'ts-jest'
 }
